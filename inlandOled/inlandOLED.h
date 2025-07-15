ac61d6eb9fd0e8422da90bf31fa4db8bbd59ca34
Arterio Rodrigues <48306622+ArterioRodrigues@users.noreply.github.com>
2025-07-14 23:36:31 -0400

    Update inlandOLED.h

--- inlandOled/inlandOLED.h
+++ inlandOled/inlandOLED.h
@@ -1,26 +1,35 @@
 #pragma once
 #include <U8g2lib.h>
 
-enum FONTSIZE {
+enum FontSize {
+    FONT_SIZE_4 = 4,
+    FONT_SIZE_5 = 5,
     FONT_SIZE_6 = 6,
     FONT_SIZE_7 = 7,
-    FONT_SIZE_8 = 8
-}
+    FONT_SIZE_8 = 8,
+    FONT_SIZE_9 = 9,
+    FONT_SIZE_10 = 10,
+    FONT_SIZE_11 = 11,
+    FONT_SIZE_12 = 12
+};
 
 class InlandOLED {
     public:
         InlandOLED(int clockPin, int dataPin, int csPin, int dcPin, int resetPin);
         void setup();
         void flipScreen(bool flip);
-        void setFont(FONTSIZE fontSize);
+        void setFont(FontSize fontSize);
         void drawStr(int x, int y, const char* str);
         void drawLine(int x0, int y0, int x1, int y1);
         void clearBuffer();
         void sendBuffer();
+        
+        uint8_t getStrWidth(const char* str);
+        uint8_t getDisplayWidth();
         ~InlandOLED();
         U8G2_SH1106_128X64_NONAME_F_4W_SW_SPI* getDisplay();
 
     private:
         U8G2_SH1106_128X64_NONAME_F_4W_SW_SPI* _u8g2;
 
-}
+};
