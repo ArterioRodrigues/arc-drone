33b50781fb335d7de604a4f86e84a4907ac63d33
Arterio Rodrigues <48306622+ArterioRodrigues@users.noreply.github.com>
2025-07-14 23:35:25 -0400

    Update controller.h

--- controller/controller.h
+++ controller/controller.h
@@ -4,13 +4,16 @@
 
 typedef void (*Callback)(void);
 
-class Controller {
-    public:
-        Controller();
-        ControllerPtr getController();
-        void processController(Callback callback);
-    private:
-        ControllerPtr controller = nullptr;
-        void onConnectedController(BP32Controller* controller);
-        void onDisconnectedController(BP32Controller* controller);
+namespace drone {
+    class Controller {
+        public:
+            Controller();
+            void setup();
+            ControllerPtr getController();
+            void processController(Callback callback);
+        private:
+            ControllerPtr controller = nullptr;
+            void onConnectedController(BP32Controller* controller);
+            void onDisconnectedController(BP32Controller* controller);
+    };
 }
