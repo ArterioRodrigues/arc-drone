166b812cfc0470dbb4b59c3aca89a363d8c95f52
Arterio Rodrigues <arterio.rodrigues@jetblue.com>
2025-07-15 00:06:26 -0400

    Inland

--- drone.ino
+++ drone.ino
@@ -5,12 +5,14 @@
 #include "esc/esc.cpp"
 #include "mpu6050/mpu6050.h"
 #include "mpu6050/mpu6050.cpp"
+#include "inlandOLED/inlandOLED.h"
+#include "inlandOLED/inlandOLED.cpp"
 
 Controller controller();
 ESC esc(DSHOT::DSHOT300);
 MPU6050 mpu6050(); //Default to GPIO 21 (SDA) and 22 (SCL)
-
-void draw() {
+InlandOLED oled(18, 19, 5, 16, 17); // Example pin numbers for clock, data, CS, DC, reset
+void draw(sensors_vec_t acceleration, sensors_vec_t gyro, float temperature) {
 
 }
 
