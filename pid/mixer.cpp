658ca16ec74fe4f50a6c5588382216f618ced013
Arterio Rodrigues <48306622+ArterioRodrigues@users.noreply.github.com>
2026-05-18 23:11:01 -0400

    Update mixer.cpp

--- pid/mixer.cpp
+++ pid/mixer.cpp
@@ -2,10 +2,10 @@
 #include <algorithm>
 Motors Mixer::compute(double base, double roll, double pitch, double yaw) {
   Motors motors;
-  motors.m1 = std::clamp(base - roll - pitch + yaw, 48.0, 2047.0);
-  motors.m2 = std::clamp(base + roll - pitch - yaw, 48.0, 2047.0);
-  motors.m3 = std::clamp(base - roll + pitch - yaw, 48.0, 2047.0);
-  motors.m4 = std::clamp(base + roll + pitch + yaw, 48.0, 2047.0);
-
+  motors.m1 = constrain(base - roll + pitch + yaw, 48.0, 2047.0);
+  motors.m2 = constrain(base + roll + pitch - yaw, 48.0, 2047.0);
+  motors.m3 = constrain(base - roll - pitch - yaw, 48.0, 2047.0);
+  motors.m4 = constrain(base + roll - pitch + yaw, 48.0, 2047.0);
+ 
   return motors;
 }
